5cfb785df3185c44242fd5acabf3de015ccd1bca
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-11-01 12:37:09 +0100

    start project

---
+++ src/pages/Community.js
@@ -0,0 +1,5 @@
+export default function Community() {
+    return (
+        <h2>Communauté</h2>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+// import Card from '../components/Card';
+
+export default function Home() {
+    return (
+        <container>
+            <div style={{textAlign:'center'}}>
+                <h2> Hello Jane</h2>
+                <p>Félicitation ! Vous avez explosé vos objectifs hier 👏🏻</p>
+            </div>
+            <div>
+            
+                {/* <Card key='cards'/> */}
+                
+            </div>
+        </container>
+    )
+}
