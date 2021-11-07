e4c5049af8400714fe95dcf128d4dde7b50fe49d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-11-07 22:41:18 +0100

    mock api test

---
+++ src/_mock_/index.jsx
@@ -0,0 +1,10 @@
+import './activity';
+import './performance';
+import './session';
+import './user';
+
+// TO DO : 
+
+// `api/user/${userId}/activity` 
+// `api/user/${userId}/average-sessions` 
+// `api/user/${userId}/performance`
---
+++ src/_mock_/user.jsx
@@ -0,0 +1,55 @@
+// utils
+// 
+import mock from './mock';
+// ------------------------
+
+mock.onGet('/api/user/profile').reply(() => {
+    const profile = {
+        id: 12,
+        userInfos: {
+            firstName: 'Karl',
+            lastName: 'Dovineau',
+            age: 31,
+        },
+        todayScore: 0.12,
+        keyData: {
+            calorieCount: 1930,
+            proteinCount: 155,
+            carbohydrateCount: 290,
+            lipidCount: 50
+        }
+    };
+// const user = [
+//     {
+//         id: 12,
+//         userInfos: {
+//             firstName: 'Karl',
+//             lastName: 'Dovineau',
+//             age: 31,
+//         },
+//         todayScore: 0.12,
+//         keyData: {
+//             calorieCount: 1930,
+//             proteinCount: 155,
+//             carbohydrateCount: 290,
+//             lipidCount: 50
+//         }
+//     },
+//     {
+//         id: 18,
+//         userInfos: {
+//             firstName: 'Cecilia',
+//             lastName: 'Ratorez',
+//             age: 34,
+//         },
+//         score: 0.3,
+//         keyData: {
+//             calorieCount: 2500,
+//             proteinCount: 90,
+//             carbohydrateCount: 150,
+//             lipidCount: 120
+//         }
+//     }
+// ]
+    return [200, { profile }];
+});
